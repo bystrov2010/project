557ac216d561fc08a351a556fbad3c895c458bb3
Vasily <bystrov2010@gmail.com>
2021-12-17 11:16:29 +0300

    17/12/2021

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
 "use strict";
 
-let incr = 10,
-    decr = 10;
+// let incr = 10,
+//     decr = 10;
+
+// console.log(incr);
+
+// const result = confirm("Are You here");
+// console.log(result);
+
+// const answer = prompt("Сколько вам лет?");
+// console.log(typeof(answer));
+
+// const category = 'toys';
+// console.log(`http://yandex.ru/${category}/5`);
+
+const isChecked = false,
+        isClose = false;
+console.log(isChecked || !isClose);
 
